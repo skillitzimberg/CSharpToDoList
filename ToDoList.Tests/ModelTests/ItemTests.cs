c0dca5947b4ea4309be136e891d0d68506e67c0d
Student <example@example.com>
2018-11-15 09:26:53 -0800

    add further tutorial code.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -42,6 +49,38 @@ namespace ToDoList.Tests
 
       //Assert
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAllItems_ReturnsEmptyList_ListWithNoItems()
+    {
+      List<Item> newList = new List<Item> {};
+
+      List<Item> result = Item.GetAllItems();
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output from empty list GetAllItems test: " + thisItem.GetDescription());
+      }
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAllItems_ReturnsPopulatedList_ListOfItems()
+    {
+      string descriptionOne = "Do a thing";
+      string descriptionTwo = "Do another thing";
+      Item newItemOne = new Item(descriptionOne);
+      Item newItemTwo = new Item(descriptionTwo);
+      List<Item> newList = new List<Item> { newItemOne, newItemTwo };
+
+      List<Item> result = Item.GetAllItems();
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output from second list GetAllItems test: " + thisItem.GetDescription());
+      }
+
+      CollectionAssert.AreEqual(newList, result);
     }
 
   }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,17 @@
+using System;
+using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     private string _description;
+    private static List<Item> _itemInstances = new List<Item> {};
 
     public Item (string description)
     {
       _description = description;
+      _itemInstances.Add(this);
     }
 
     public string GetDescription()
@@ -18,6 +22,16 @@ namespace ToDoList.Models
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+
+    public static List<Item> GetAllItems()
+    {
+      return _itemInstances;
+    }
+
+    public static void ClearAll()
+    {
+      _itemInstances.Clear();
     }
 
   }
